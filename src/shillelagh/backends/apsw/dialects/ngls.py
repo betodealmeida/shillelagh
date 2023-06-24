c9079f2d0e25d01a7c2793570641a404576d4862
Geert Fieremans <geert.fieremans@unify.com>
2023-06-24 12:04:04 -0400

    Add superset parameter when calling NGLS reporting service reports (#30)
    * Add use_timezone parameter when calling NGLS reporting service
    
    * Add superset parameter when calling NGLS reporting service for individual report
    
    * Safe guard params
    
    * Run pytest as part of commit

--- src/shillelagh/backends/apsw/dialects/ngls.py
+++ src/shillelagh/backends/apsw/dialects/ngls.py
@@ -66,7 +66,8 @@ class NglsReports:
         """Get a list of all NGLS reports and populate a Pandas table."""
         try:
             headers = {"X-NGLS-API-Key": self.api_key}
-            url = f"https://{self.host}:{self.port}/{self.database}/v1/reports?all_reports=true&superset=true"  # pylint: disable=line-too-long
+            # pylint: disable=line-too-long
+            url = f"{self.url()}?all_reports=true&superset=true"
             response = requests.get(
                 url,
                 headers=headers,
@@ -175,6 +176,10 @@ class NglsReports:
         """Perform a GET request to the reporting service."""
         headers = {"X-NGLS-API-Key": self.api_key}
         url = self.url(tablename)
+        if not params:
+            params = {"superset": "true"}
+        else:
+            params["superset"] = "true"
         _logger.info(f"Get report from NGLS: GET {url} {json.dumps(params)}")
         response = requests.get(
             url,
