c89972a588a1b6f289ad4d49b1c926049a5ed2c7
Beto Dealmeida <roberto@dealmeida.net>
2023-07-13 17:18:50 -0700

    feat: combined system resource adapter

--- src/shillelagh/adapters/api/system.py
+++ src/shillelagh/adapters/api/system.py
@@ -8,12 +8,12 @@ import logging
 import time
 import urllib.parse
 from datetime import datetime, timezone
-from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
+from enum import Enum
+from typing import Any, Dict, Iterator, List, Optional, Set, Tuple, Type, Union
 
 import psutil
 
 from shillelagh.adapters.base import Adapter
-from shillelagh.exceptions import ProgrammingError
 from shillelagh.fields import DateTime, Field, Float, Integer, Order
 from shillelagh.filters import Filter
 from shillelagh.typing import RequestedOrder, Row
@@ -21,17 +21,83 @@ from shillelagh.typing import RequestedOrder, Row
 _logger = logging.getLogger(__name__)
 
 AVERAGE_NUMBER_OF_ROWS = 100
-MEMORY_COLUMNS = [
-    "total",
-    "available",
-    "percent",
-    "used",
-    "free",
-    "active",
-    "inactive",
-    "wired",
-]
-SWAP_COLUMNS = ["total", "used", "free", "percent", "sin", "sout"]
+
+
+MEMORY_COLUMNS: Dict[str, Type[Field]] = {
+    "total": Integer,
+    "available": Integer,
+    "percent": Float,
+    "used": Integer,
+    "free": Integer,
+    "active": Integer,
+    "inactive": Integer,
+    "wired": Integer,
+}
+SWAP_COLUMNS: Dict[str, Type[Field]] = {
+    "total": Integer,
+    "used": Integer,
+    "free": Integer,
+    "percent": Float,
+    "sin": Integer,
+    "sout": Integer,
+}
+
+
+class ResourceType(str, Enum):
+    """
+    The type of resource to retrieve.
+    """
+
+    CPU = "cpu"
+    MEMORY = "memory"
+    SWAP = "swap"
+    ALL = "all"
+
+
+def get_columns(resource: ResourceType) -> Dict[str, Field]:
+    """
+    Build columns depending on the chosen resource.
+    """
+    memory_prefix = "virtual_" if resource == ResourceType.ALL else ""
+    swap_prefix = "swap_" if resource == ResourceType.ALL else ""
+
+    columns: Dict[str, Field] = {}
+
+    if resource in {ResourceType.CPU, ResourceType.ALL}:
+        columns.update(
+            {
+                f"cpu{i}": Float(
+                    filters=None,
+                    order=Order.NONE,
+                    exact=False,
+                )
+                for i in range(psutil.cpu_count())
+            },
+        )
+    if resource in {ResourceType.MEMORY, ResourceType.ALL}:
+        columns.update(
+            {
+                f"{memory_prefix}{column}": field(
+                    filters=None,
+                    order=Order.NONE,
+                    exact=False,
+                )
+                for column, field in MEMORY_COLUMNS.items()
+            },
+        )
+    if resource in {ResourceType.SWAP, ResourceType.ALL}:
+        columns.update(
+            {
+                f"{swap_prefix}{column}": field(
+                    filters=None,
+                    order=Order.NONE,
+                    exact=False,
+                )
+                for column, field in SWAP_COLUMNS.items()
+            },
+        )
+
+    return columns
 
 
 class SystemAPI(Adapter):
@@ -44,16 +110,21 @@ class SystemAPI(Adapter):
 
     supports_limit = True
     supports_offset = True
+    supports_requested_columns = True
 
     @staticmethod
     def supports(uri: str, fast: bool = True, **kwargs: Any) -> Optional[bool]:
         parsed = urllib.parse.urlparse(uri)
-        return parsed.scheme == "system"
+        return parsed.scheme == "system" and (
+            # pylint: disable=protected-access
+            parsed.netloc in ResourceType._value2member_map_
+            or parsed.netloc == ""
+        )
 
     @staticmethod
     def parse_uri(uri: str) -> Union[Tuple[str], Tuple[str, float]]:
         parsed = urllib.parse.urlparse(uri)
-        resource = parsed.netloc
+        resource = parsed.netloc or "all"
         query_string = urllib.parse.parse_qs(parsed.query)
 
         if "interval" in query_string:
@@ -63,92 +134,105 @@ class SystemAPI(Adapter):
     def __init__(self, resource: str, interval: float = 1.0):
         super().__init__()
 
-        self.resource = resource
+        self.resource = ResourceType(resource)
         self.interval = interval
 
         self._set_columns()
-
-    def _get_memory_column(self, column: str) -> Field:
-        """
-        Return a column field given its name.
-
-        All memory columns are integers (bytes), except for ``percent``.
-        """
-        if column == "percent":
-            return Float(filters=None, order=Order.NONE, exact=False)
-
-        return Integer(filters=None, order=Order.NONE, exact=False)
 
     def _set_columns(self) -> None:
         self.columns: Dict[str, Field] = {
             "timestamp": DateTime(filters=None, order=Order.ASCENDING, exact=False),
         }
-
-        if self.resource == "cpu":
-            self.columns.update(
-                {
-                    f"cpu{i}": Float(
-                        filters=None,
-                        order=Order.NONE,
-                        exact=False,
-                    )
-                    for i in range(psutil.cpu_count())
-                },
-            )
-        elif self.resource == "memory":
-            self.columns.update(
-                {column: self._get_memory_column(column) for column in MEMORY_COLUMNS},
-            )
-        elif self.resource == "swap":
-            self.columns.update(
-                {column: self._get_memory_column(column) for column in SWAP_COLUMNS},
-            )
-        else:
-            raise ProgrammingError(f"Unknown resource: {self.resource}")
-
-        self.columns["timestamp"] = DateTime(
-            filters=None,
-            order=Order.ASCENDING,
-            exact=False,
-        )
+        self.columns.update(get_columns(self.resource))
 
     def get_columns(self) -> Dict[str, Field]:
         return self.columns
 
-    def get_data(
+    def get_data(  # pylint: disable=too-many-arguments
         self,
         bounds: Dict[str, Filter],
         order: List[Tuple[str, RequestedOrder]],
         limit: Optional[int] = None,
         offset: Optional[int] = None,
+        requested_columns: Optional[Set[str]] = None,
         **kwargs: Any,
     ) -> Iterator[Row]:
+        requested_columns = requested_columns or set(self.columns.keys())
+
         rowid = 0
         while limit is None or rowid < limit:
             if offset is not None:
                 time.sleep(self.interval * offset)
 
-            if self.resource == "cpu":
+            row: Dict[str, Any] = {"rowid": rowid}
+            if "timestamp" in requested_columns:
+                row["timestamp"] = datetime.now(timezone.utc)
+
+            needs_sleep = True
+
+            # add CPU?
+            if any(column.startswith("cpu") for column in requested_columns):
                 try:
-                    row = {
-                        f"cpu{i}": value / 100.0
-                        for i, value in enumerate(
-                            psutil.cpu_percent(interval=self.interval, percpu=True),
-                        )
-                    }
+                    row.update(
+                        {
+                            f"cpu{i}": value / 100.0
+                            for i, value in enumerate(
+                                psutil.cpu_percent(interval=self.interval, percpu=True),
+                            )
+                            if f"cpu{i}" in requested_columns
+                        },
+                    )
+                    needs_sleep = False
                 except KeyboardInterrupt:
                     return
-            elif self.resource == "memory":
-                row = psutil.virtual_memory()._asdict()
-                time.sleep(self.interval)
-            elif self.resource == "swap":
-                row = psutil.swap_memory()._asdict()
-                time.sleep(self.interval)
-            else:
-                raise ProgrammingError(f"Unknown resource: {self.resource}")
 
-            row.update({"rowid": rowid, "timestamp": datetime.now(timezone.utc)})
+            # virtual memory
+            if self.resource == ResourceType.ALL and any(
+                column.startswith("virtual_") for column in requested_columns
+            ):
+                row.update(
+                    {
+                        f"virtual_{k}": v
+                        for k, v in psutil.virtual_memory()._asdict().items()
+                        if f"virtual_{k}" in requested_columns
+                    },
+                )
+            elif self.resource == ResourceType.MEMORY and any(
+                column in MEMORY_COLUMNS for column in requested_columns
+            ):
+                row.update(
+                    {
+                        k: v
+                        for k, v in psutil.virtual_memory()._asdict().items()
+                        if k in requested_columns
+                    },
+                )
+
+            # swap
+            if self.resource == ResourceType.ALL and any(
+                column.startswith("swap_") for column in requested_columns
+            ):
+                row.update(
+                    {
+                        f"swap_{k}": v
+                        for k, v in psutil.swap_memory()._asdict().items()
+                        if f"swap_{k}" in requested_columns
+                    },
+                )
+            elif self.resource == ResourceType.SWAP and any(
+                column in SWAP_COLUMNS for column in requested_columns
+            ):
+                row.update(
+                    {
+                        k: v
+                        for k, v in psutil.swap_memory()._asdict().items()
+                        if k in requested_columns
+                    },
+                )
 
             _logger.debug(row)
             yield row
             rowid += 1
+
+            if needs_sleep:
+                time.sleep(self.interval)
--- tests/adapters/api/system_test.py
+++ tests/adapters/api/system_test.py
@@ -4,13 +4,11 @@ Tests for the system adapter.
 from datetime import datetime, timezone
 from unittest import mock
 
-import pytest
 from freezegun import freeze_time
 from pytest_mock import MockerFixture
 
 from shillelagh.adapters.api.system import SystemAPI
 from shillelagh.backends.apsw.db import connect
-from shillelagh.exceptions import ProgrammingError
 
 
 def test_system_cpu(mocker: MockerFixture) -> None:
@@ -157,21 +155,6 @@ def test_system_different_interval(mocker: MockerFixture) -> None:
     )
 
 
-def test_system_invalid_resource() -> None:
-    """
-    Test a query referencing an invalid resource.
-    """
-    connection = connect(":memory:")
-    cursor = connection.cursor()
-    sql = """
-        SELECT * FROM "system://disk"
-        LIMIT 2
-    """
-    with pytest.raises(ProgrammingError) as excinfo:
-        list(cursor.execute(sql))
-    assert str(excinfo.value) == "Unknown resource: disk"
-
-
 def test_system_interrupt(mocker: MockerFixture) -> None:
     """
     Test CPU query interrupt.
@@ -197,12 +180,10 @@ def test_system_interrupt(mocker: MockerFixture) -> None:
     ]
 
 
-def test_get_data(mocker: MockerFixture) -> None:
+def test_get_data_cpu(mocker: MockerFixture) -> None:
     """
-    Test ``get_data``.
+    Test ``get_data`` with the ``CPU`` resource.
     """
-    adapter = SystemAPI("cpu")
-
     psutil = mocker.patch("shillelagh.adapters.api.system.psutil")
     psutil.cpu_count.return_value = 4
     psutil.cpu_percent.side_effect = [
@@ -212,6 +193,8 @@ def test_get_data(mocker: MockerFixture) -> None:
         [1, 2, 3, 4],
     ]
     time = mocker.patch("shillelagh.adapters.api.system.time")
+
+    adapter = SystemAPI("cpu")
 
     with freeze_time("2021-01-01T00:00:00Z"):
         data = list(adapter.get_data({}, [], limit=2, offset=1))
@@ -236,7 +219,119 @@ def test_get_data(mocker: MockerFixture) -> None:
 
     time.sleep.assert_called_with(1.0)
 
-    adapter.resource = "bogus"
-    with pytest.raises(ProgrammingError) as excinfo:
-        list(adapter.get_data({}, [], limit=2, offset=1))
-    assert str(excinfo.value) == "Unknown resource: bogus"
+    data = list(adapter.get_data({}, [], limit=2, offset=1, requested_columns={"cpu1"}))
+    assert data == [
+        {"rowid": 0, "cpu1": 0.02},
+        {"rowid": 1, "cpu1": 0.02},
+    ]
+
+
+def test_get_data_all(mocker: MockerFixture) -> None:
+    """
+    Test ``get_data`` with the ``ALL`` resource.
+    """
+    psutil = mocker.patch("shillelagh.adapters.api.system.psutil")
+    psutil.cpu_count.return_value = 4
+    psutil.cpu_percent.return_value = [1, 2, 3, 4]
+    psutil.virtual_memory()._asdict.return_value = {
+        "total": 34359738368,
+        "available": 15130095616,
+        "percent": 56.0,
+        "used": 18285113344,
+        "free": 1579941888,
+        "active": 13551853568,
+        "inactive": 13460545536,
+        "wired": 4733259776,
+    }
+    psutil.swap_memory()._asdict.return_value = {
+        "total": 18253611008,
+        "used": 16865034240,
+        "free": 1388576768,
+        "percent": 92.4,
+        "sin": 1010873262080,
+        "sout": 4259106816,
+    }
+    time = mocker.patch("shillelagh.adapters.api.system.time")
+
+    adapter = SystemAPI("all")
+
+    with freeze_time("2021-01-01T00:00:00Z"):
+        data = list(adapter.get_data({}, [], limit=1))
+    assert data == [
+        {
+            "rowid": 0,
+            "timestamp": datetime(2021, 1, 1, 0, 0, tzinfo=timezone.utc),
+            "cpu0": 0.01,
+            "cpu1": 0.02,
+            "cpu2": 0.03,
+            "cpu3": 0.04,
+            "virtual_total": 34359738368,
+            "virtual_available": 15130095616,
+            "virtual_percent": 56.0,
+            "virtual_used": 18285113344,
+            "virtual_free": 1579941888,
+            "virtual_active": 13551853568,
+            "virtual_inactive": 13460545536,
+            "virtual_wired": 4733259776,
+            "swap_total": 18253611008,
+            "swap_used": 16865034240,
+            "swap_free": 1388576768,
+            "swap_percent": 92.4,
+            "swap_sin": 1010873262080,
+            "swap_sout": 4259106816,
+        },
+    ]
+
+    time.sleep.assert_not_called()
+
+    psutil.cpu_percent.reset_mock()
+    psutil.virtual_memory.reset_mock()
+    psutil.swap_memory.reset_mock()
+
+    with freeze_time("2021-01-01T00:00:00Z"):
+        data = list(
+            adapter.get_data(
+                {},
+                [],
+                limit=1,
+                requested_columns={"virtual_percent"},
+            ),
+        )
+    assert data == [{"rowid": 0, "virtual_percent": 56.0}]
+
+    psutil.cpu_percent.assert_not_called()
+    psutil.virtual_memory.assert_called()
+    psutil.swap_memory.assert_not_called()
+
+    time.sleep.assert_called_with(1.0)
+
+
+def test_get_data_bogus(mocker: MockerFixture) -> None:
+    """
+    Test ``get_data`` with a bogus ``requested_columns``.
+    """
+    mocker.patch("shillelagh.adapters.api.system.time")
+
+    with freeze_time("2021-01-01T00:00:00Z"):
+        data = list(
+            SystemAPI("all").get_data({}, [], limit=1, requested_columns={"bogus"}),
+        )
+    assert data == [{"rowid": 0}]
+
+    with freeze_time("2021-01-01T00:00:00Z"):
+        data = list(
+            SystemAPI("cpu").get_data({}, [], limit=1, requested_columns={"bogus"}),
+        )
+    assert data == [{"rowid": 0}]
+
+    with freeze_time("2021-01-01T00:00:00Z"):
+        data = list(
+            SystemAPI("memory").get_data({}, [], limit=1, requested_columns={"bogus"}),
+        )
+    assert data == [{"rowid": 0}]
+
+    with freeze_time("2021-01-01T00:00:00Z"):
+        data = list(
+            SystemAPI("swap").get_data({}, [], limit=1, requested_columns={"bogus"}),
+        )
+    assert data == [{"rowid": 0}]
