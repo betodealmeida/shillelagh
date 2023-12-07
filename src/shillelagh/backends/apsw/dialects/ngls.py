a43a56168cdb6c30916c1029307fc71502151db7
EduardoSanday <117779587+EduardoSanday@users.noreply.github.com>
2023-12-07 18:19:15 -0300

    Support TDoS reports (#33)
    * add tdos report
    
    * update

--- src/shillelagh/backends/apsw/dialects/ngls.py
+++ src/shillelagh/backends/apsw/dialects/ngls.py
@@ -83,8 +83,9 @@ class NglsReports:
             )
             return
         reports_df = pd.json_normalize(response.json())
-        # filter out the Gemma reports
-        reports_df = reports_df[reports_df["report.category"] == "Gemma"]
+        # filter out the Gemma and TDoS reports
+        # TODO: create a generic filter to separate what is Gemma, TDoS...
+        reports_df = reports_df[(reports_df["report.category"] == "Gemma") | (reports_df["report.category"] == "Tdos")]
         self.table_names = list(reports_df["table_name"].values)
         table_ids = dict(
             reports_df[["table_name", "report.id"]]
