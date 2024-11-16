7d4b9e55df5e858d6c4cf2f79c335b31b12a1050
Kirill Nikolaevskiy <happykust@list.ru>
2024-11-16 22:19:04 +0300

    fix(tests): add close method to generic_xml adapter

--- src/shillelagh/adapters/api/generic_xml.py
+++ src/shillelagh/adapters/api/generic_xml.py
@@ -10,6 +10,7 @@ from typing import Any, Optional
 from defusedxml import ElementTree as DET
 
 from shillelagh.adapters.api.generic_json import GenericJSONAPI
+from shillelagh.adapters.base import current_network_resource
 from shillelagh.filters import Filter
 from shillelagh.lib import flatten
 from shillelagh.typing import RequestedOrder, Row
@@ -78,3 +79,9 @@ class GenericXMLAPI(GenericJSONAPI):
             row["rowid"] = i
             _logger.debug(row)
             yield flatten(row)
+
+    def close(self) -> None:
+        """
+        Unset network resource
+        """
+        current_network_resource.set(None)
